cd0c374fa64993ce75f1197d2bf4ec40d642a89b
Максм <m.ustich@istu.edu.ua>
2026-03-24 21:26:57 +0200

    додано практичну 8

---
+++ Lab_Exception_Vect/Lab_Exception_Vect/Lab_Exception_Vect.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+using namespace std;
+
+class Vect
+{
+public:
+    Vect(int n) : size(n), p(nullptr)
+    {
+        try
+        {
+            if (size <= 0 || size > 256)
+                throw "Невірний розмір масиву!";
+
+            p = new int[size];
+
+            for (int i = 0; i < size; ++i)
+                p[i] = 0;
+        }
+        catch (...)
+        {
+            cout << "Помилка у конструкторі Vect" << endl;
+            throw;
+        }
+    }
+
+    ~Vect()
+    {
+        try
+        {
+            delete[] p;
+        }
+        catch (...)
+        {
+            cout << "Помилка у деструкторі Vect" << endl;
+        }
+    }
+
+    int& operator[](int i)
+    {
+        if (i < 0 || i >= size)
+            throw "Індекс поза межами масиву!";
+        return p[i];
+    }
+
+    void Print()
+    {
+        for (int i = 0; i < size; ++i)
+            cout << p[i] << " ";
+        cout << endl;
+    }
+
+private:
+    int* p;
+    int size;
+};
+
+int main()
+{
+    try
+    {
+        Vect a(3);
+        a[0] = 0;
+        a[1] = 1;
+        a[2] = 2;
+        a.Print();
+
+        Vect a1(200);
+        a1[10] = 5;
+        a1.Print();
+    }
+    catch (const char* s)
+    {
+        cout << "Помилка: " << s << endl;
+    }
+    catch (...)
+    {
+        cout << "Невідома помилка!" << endl;
+    }
+
+    return 0;
+}
